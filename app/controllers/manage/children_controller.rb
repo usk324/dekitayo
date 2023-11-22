3ae1d7d00de35ed36a7ae022992e0ecaccd82625
Mao Christie <maochr@gmail.com>
2023-11-22 15:34:09 +0900

    added a form to add child

--- app/controllers/manage/children_controller.rb
+++ app/controllers/manage/children_controller.rb
@@ -1,13 +1,28 @@
 class Manage::ChildrenController < ApplicationController
   def new
+    @child = Child.new()
   end
 
   def create
+    @child = Child.new(child_params)
+    @child.user = current_user
+
+    if @child.save
+      redirect_to manage_child_path(@child.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
   end
 
   def index
+  end
+
+  private
+
+  def child_params
+    params.require(:child).permit(:name, :date_of_birth, :photo)
   end
 end
