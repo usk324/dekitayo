2fe68e0a097a9e94be1fec2cd47871c7689719ff
Yusuke Mori <usk324@gmail.com>
2023-11-23 11:15:24 +0900

    change the layout of child show page

--- app/controllers/manage/children_controller.rb
+++ app/controllers/manage/children_controller.rb
@@ -7,6 +7,11 @@ class Manage::ChildrenController < ApplicationController
   end
 
   def show
+    @current_time = Time.now
+    @child = Child.find(params[:id])
+    url = "https://api.openweathermap.org/data/2.5/weather?lat=35.6764&lon=139.6500&appid=68c7d9645dcea9731a25be00174dd457"
+    serialized = URI.open(url).read
+    @weather = JSON.parse(serialized)
   end
 
 
