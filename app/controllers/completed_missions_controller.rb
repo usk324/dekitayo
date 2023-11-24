415b3764cda28ce7e5856cc93d00a026178d6ab1
Mao Christie <maochr@gmail.com>
2023-11-24 16:21:17 +0900

    edited game board

--- app/controllers/completed_missions_controller.rb
+++ app/controllers/completed_missions_controller.rb
@@ -2,6 +2,7 @@ class CompletedMissionsController < GamesController
   def create
     @mission = current_child.missions.find(params[:mission_id])
     @mission.update(status: "finished")
+    current_child.update(coins: current_child.coins += @mission.coins)
     @completed_mission = CompletedMission.new(mission: @mission, completed_at: Time.now)
     @completed_mission.save
     flash[:notice] = 'Mission finished successfully!'
