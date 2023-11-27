ddd2ab20030443b5607320149030abea07305fcb
Mao Christie <maochr@gmail.com>
2023-11-27 13:39:57 +0900

    working on charts and creating usable data

--- app/models/child.rb
+++ app/models/child.rb
@@ -14,4 +14,35 @@ class Child < ApplicationRecord
     self.position = position + 1
     save
   end
+
+  def completed_missions_data
+   data = self.missions
+   .where(status: "finished")
+   .where(date: ((Date.today - 30.days)..Date.today))
+   .group("date").count
+   (Date.today - 30.days).upto(Date.today) do |date|
+    if data[date]
+       next
+     else
+      data[date] = 0
+     end
+    end
+    return data
+  end
+
+  def assigned_missions_data
+    data = self.missions
+    .where(date: ((Date.today - 30.days)..Date.today))
+    .group("date").count
+    (Date.today - 30.days).upto(Date.today) do |date|
+      puts date
+     if data[date]
+        next
+      else
+       data[date] = 0
+      end
+     end
+     return data
+  end
+
 end
