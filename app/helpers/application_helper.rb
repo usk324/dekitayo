c29f518a6987cf403c3c8566f3ebeda0f10d2db1
Ashamarina Uchida <ashamarinauchida@me.com>
2023-11-27 17:32:13 +0900

    placed game tiles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def current_child
     @current_child
   end
+
+  def status_color_class(status)
+    case status
+    when "completed"
+      "completed-status"
+    when "started"
+      "started-status"
+    else
+      ""
+    end
+  end
+
 end
