af8ffe90a1155a7d26dcf9463d15a509f69dfd55
Samantha Stone <samstone1171@gmail.com>
2023-11-27 20:15:05 +0900

    fixed issue with child rewards display

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,7 +1,6 @@
 class RewardsController < GamesController
   def index
     @random_images = ["Reward-icon-1.png", "Reward-icon-2.png", "Reward-icon-3.png", "Reward-icon-4.png", "Reward-icon-5.png", "Reward-icon-6.png", "Reward-icon-7.png", "Reward-icon-8.png"]
-    @child = Child.find(params[:format])
-    @rewards = Reward.where(child: @child)
+    @rewards = Reward.where(child: current_child)
   end
 end
