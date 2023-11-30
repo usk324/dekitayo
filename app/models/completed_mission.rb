43858749b5527be52eb68a0fe2eeed4ff4a9ef95
Mao Christie <maochr@gmail.com>
2023-11-30 15:30:38 +0900

    added map display with marker of last location of child to show page

--- app/models/completed_mission.rb
+++ app/models/completed_mission.rb
@@ -2,6 +2,6 @@ class CompletedMission < ApplicationRecord
   belongs_to :mission
   has_one :child, through: :mission
   has_one_attached :photo
-
+  geocoded_by :completed_latitude
   validates :completed_at, presence: true
 end
