aaee4e99b8a9ab313b66b2baf7c40c64a6e7060c
Samantha Stone <samstone1171@gmail.com>
2023-11-30 16:01:53 +0900

    sound when buying a reward

--- app/javascript/controllers/purchase_reward_controller.js
+++ app/javascript/controllers/purchase_reward_controller.js
@@ -2,10 +2,39 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="purchase-reward"
 export default class extends Controller {
+  static values = {
+    src: String,
+    play: Boolean
+  }
+
   connect() {
-    console.log("hello")
+    // console.log(this.playValue);
+    // console.log(this.playValue.getClass());
+    if (this.playValue === true) {
+      // console.log("It should be playing");
+      const soundPath = this.srcValue;
+      // console.log('Sound Path:', soundPath);
+
+      if (!soundPath) {
+          console.error('Audio file path is not available.');
+          return;
+      }
+
+      const audio = new Audio(soundPath);
+      audio.play();
+    }
   }
-  purchase(event) {
-    console.log("hello");
-  }
+
+  // play() {
+  //   const soundPath = this.srcValue;
+  //   console.log('Sound Path:', soundPath);
+
+  //   if (!soundPath) {
+  //       console.error('Audio file path is not available.');
+  //       return;
+  //   }
+
+  //   const audio = new Audio(soundPath);
+  //   audio.play();
+  // }
 }
